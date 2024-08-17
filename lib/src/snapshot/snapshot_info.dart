87cceb822990f8ae2a011c3d681fa837bf1a4cd8
Piotr Arkhipenka <arhipenkoqwerty@gmail.com>
2024-08-17 17:36:21 +0200

    Add documentation for snapshot_info.dart

--- lib/src/snapshot/snapshot_info.dart
+++ lib/src/snapshot/snapshot_info.dart
@@ -1,11 +1,22 @@
 import 'dart:ui';
 
+/// Information about the snapshot, which is taken before the effect starts
+/// This information is used to render the effect
 class SnapshotInfo {
+
+  /// The snapshot of the child widget, which is passed to the shader
   final Image image;
+
+  /// The width of the snapshot
   final double width;
+
+  /// The height of the snapshot
   final double height;
+
+  /// The global position of the snapshot
   final Offset position;
 
+  /// Creates a SnapshotInfo object
   SnapshotInfo(
     this.image,
     this.width,
